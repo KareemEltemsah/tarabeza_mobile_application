58ee311eedcb6b6ed538c60db85723795ced5d63
Kareem Eltemsah <36171346+KareemEltemsah@users.noreply.github.com>
2021-04-23 07:49:15 +0200

    adding restaurant profile pt.1

---
+++ lib/src/pages/details.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import 'package:mvc_pattern/mvc_pattern.dart';
+
+import '../../generated/l10n.dart';
+import '../controllers/restaurant_controller.dart';
+import '../elements/CircularLoadingWidget.dart';
+import '../elements/DrawerWidget.dart';
+
+// import '../elements/PermissionDeniedWidget.dart';
+import 'restaurant.dart';
+import '../models/route_argument.dart';
+import '../repository/user_repository.dart';
+
+// import 'map.dart';
+import 'menu_list.dart';
+import '../models/restaurant.dart';
+
+class DetailsWidget extends StatefulWidget {
+  RouteArgument routeArgument;
+  dynamic currentTab;
+  final GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
+  Widget currentPage;
+
+  DetailsWidget({
+    Key key,
+    this.currentTab,
+  }) {
+    if (currentTab != null) {
+      if (currentTab is RouteArgument) {
+        routeArgument = currentTab;
+        currentTab = int.parse(currentTab.id);
+      }
+    } else {
+      currentTab = 0;
+    }
+  }
+
+  @override
+  _DetailsWidgetState createState() {
+    return _DetailsWidgetState();
+  }
+}
+
+class _DetailsWidgetState extends StateMVC<DetailsWidget> {
+  RestaurantController _con;
+
+  _DetailsWidgetState() : super(RestaurantController()) {
+    _con = controller;
+  }
+
+  initState() {
+    _selectTab(widget.currentTab);
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(DetailsWidget oldWidget) {
+    _selectTab(oldWidget.currentTab);
+    super.didUpdateWidget(oldWidget);
+  }
+
+  void _selectTab(int tabItem) {
+    setState(() {
+      widget.currentTab = tabItem;
+      switch (tabItem) {
+        case 0:
+          _con.getRestaurant(id: widget.routeArgument.param).then((value) {
+            setState(() {
+              // print("rest map");
+              // print(_con.restaurant.toMap());
+              // print("items map");
+              // print(_con.items.map((e) => e.toMap()));
+              // print("categories map");
+              // print(_con.categories.map((e) => e.toMap()));
+              widget.currentPage = RestaurantWidget(
+                  parentScaffoldKey: widget.scaffoldKey,
+                  routeArgument: RouteArgument(param: _con.restaurant));
+            });
+          });
+          break;
+        // case 1:
+        //   if (currentUser.value.apiToken == null) {
+        //     widget.currentPage = PermissionDeniedWidget();
+        //   }
+        //   break;
+        // case 2:
+        //   widget.currentPage = MapWidget(parentScaffoldKey: widget.scaffoldKey, routeArgument: RouteArgument(param: _con.restaurant));
+        //   break;
+        case 3:
+          widget.currentPage = MenuWidget(parentScaffoldKey: widget.scaffoldKey, routeArgument: RouteArgument(param: _con.restaurant));
+          break;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        key: widget.scaffoldKey,
+        drawer: DrawerWidget(),
+        bottomNavigationBar: Container(
+          height: 66,
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            boxShadow: [
+              BoxShadow(
+                  color: Theme.of(context).hintColor.withOpacity(0.10),
+                  offset: Offset(0, -4),
+                  blurRadius: 10)
+            ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                icon: Icon(
+                  Icons.store,
+                  size: widget.currentTab == 0 ? 28 : 24,
+                  color: widget.currentTab == 0
+                      ? Theme.of(context).accentColor
+                      : Theme.of(context).focusColor,
+                ),
+                onPressed: () {
+                  this._selectTab(0);
+                },
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.rate_review_rounded,
+                  size: widget.currentTab == 1 ? 28 : 24,
+                  color: widget.currentTab == 1
+                      ? Theme.of(context).accentColor
+                      : Theme.of(context).focusColor,
+                ),
+                onPressed: () {
+                  this._selectTab(1);
+                },
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.directions,
+                  size: widget.currentTab == 2 ? 28 : 24,
+                  color: widget.currentTab == 2
+                      ? Theme.of(context).accentColor
+                      : Theme.of(context).focusColor,
+                ),
+                onPressed: () {
+                  this._selectTab(2);
+                },
+              ),
+              FlatButton(
+                onPressed: () {
+                  this._selectTab(3);
+                },
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                shape: StadiumBorder(),
+                color: Theme.of(context).accentColor,
+                child: Wrap(
+                  spacing: 10,
+                  children: [
+                    Icon(Icons.restaurant,
+                        color: Theme.of(context).primaryColor),
+                    Text(
+                      S.of(context).menu,
+                      style: TextStyle(color: Theme.of(context).primaryColor),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: widget.currentPage ?? CircularLoadingWidget(height: 400));
+  }
+}
---
+++ lib/src/pages/restaurant.dart
@@ -0,0 +1,367 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:global_configuration/global_configuration.dart';
+import 'package:mvc_pattern/mvc_pattern.dart';
+import 'package:tarabeza_mobile_application/src/elements/SearchWidget.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../../generated/l10n.dart';
+import '../controllers/restaurant_controller.dart';
+import '../elements/CircularLoadingWidget.dart';
+import '../helpers/helper.dart';
+import '../models/restaurant.dart';
+import '../models/route_argument.dart';
+import '../repository/settings_repository.dart';
+import '../repository/user_repository.dart';
+
+class RestaurantWidget extends StatefulWidget {
+  final RouteArgument routeArgument;
+  final GlobalKey<ScaffoldState> parentScaffoldKey;
+
+  RestaurantWidget({Key key, this.parentScaffoldKey, this.routeArgument})
+      : super(key: key);
+
+  @override
+  _RestaurantWidgetState createState() {
+    return _RestaurantWidgetState();
+  }
+}
+
+class _RestaurantWidgetState extends StateMVC<RestaurantWidget> {
+  RestaurantController _con;
+
+  _RestaurantWidgetState() : super(RestaurantController()) {
+    _con = controller;
+  }
+
+  @override
+  void initState() {
+    _con.restaurant = widget.routeArgument.param as Restaurant;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        key: _con.scaffoldKey,
+        body: RefreshIndicator(
+          onRefresh: _con.refreshRestaurant,
+          child: _con.restaurant == null
+              ? CircularLoadingWidget(height: 500)
+              : Stack(
+                  fit: StackFit.expand,
+                  children: <Widget>[
+                    CustomScrollView(
+                      primary: true,
+                      shrinkWrap: false,
+                      slivers: <Widget>[
+                        SliverAppBar(
+                          backgroundColor:
+                              Theme.of(context).accentColor.withOpacity(0.9),
+                          expandedHeight: 300,
+                          elevation: 0,
+//                          iconTheme: IconThemeData(color: Theme.of(context).primaryColor),
+                          automaticallyImplyLeading: false,
+                          leading: new IconButton(
+                            icon: new Icon(Icons.sort,
+                                color: Theme.of(context).primaryColor),
+                            onPressed: () => widget
+                                .parentScaffoldKey.currentState
+                                .openDrawer(),
+                          ),
+                          flexibleSpace: FlexibleSpaceBar(
+                            collapseMode: CollapseMode.parallax,
+                            background: Hero(
+                              tag: (widget?.routeArgument?.heroTag ?? '') +
+                                  _con.restaurant.id,
+                              child: Image.network(
+                                '${GlobalConfiguration().getValue('logos_base_url')}${_con.restaurant.logo_url}',
+                                fit: BoxFit.cover,
+                                errorBuilder: (BuildContext context,
+                                    Object exception, StackTrace stackTrace) {
+                                  return Icon(Icons.error);
+                                },
+                              ),
+                              /*CachedNetworkImage(
+                                fit: BoxFit.cover,
+                                imageUrl:
+                                    '${GlobalConfiguration().getValue('logos_base_url')}${_con.restaurant.logo_url}',
+                                placeholder: (context, url) => Image.asset(
+                                  'assets/img/loading.gif',
+                                  fit: BoxFit.cover,
+                                ),
+                                errorWidget: (context, url, error) =>
+                                    Icon(Icons.error),
+                              ),*/
+                            ),
+                          ),
+                        ),
+                        SliverToBoxAdapter(
+                          child: Wrap(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    right: 20, left: 20, bottom: 10, top: 25),
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Expanded(
+                                      child: Text(
+                                        _con.restaurant?.name ?? '',
+                                        overflow: TextOverflow.fade,
+                                        softWrap: false,
+                                        maxLines: 2,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headline3,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 32,
+                                      child: Chip(
+                                        padding: EdgeInsets.all(0),
+                                        label: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: <Widget>[
+                                            Text(_con.restaurant.rating,
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .bodyText1
+                                                    .merge(TextStyle(
+                                                        color: Theme.of(context)
+                                                            .primaryColor))),
+                                            Icon(
+                                              Icons.star_border,
+                                              color: Theme.of(context)
+                                                  .primaryColor,
+                                              size: 16,
+                                            ),
+                                          ],
+                                        ),
+                                        backgroundColor: Theme.of(context)
+                                            .accentColor
+                                            .withOpacity(0.9),
+                                        shape: StadiumBorder(),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  SizedBox(width: 20),
+                                  Container(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 12, vertical: 3),
+                                    decoration: BoxDecoration(
+                                        color: _con.restaurant.isClosed()
+                                            ? Colors.red
+                                            : Colors.green,
+                                        borderRadius:
+                                            BorderRadius.circular(24)),
+                                    child: _con.restaurant.isClosed()
+                                        ? Text(
+                                            S.of(context).closed,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .caption
+                                                .merge(TextStyle(
+                                                    color: Theme.of(context)
+                                                        .primaryColor)),
+                                          )
+                                        : Text(
+                                            S.of(context).open,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .caption
+                                                .merge(TextStyle(
+                                                    color: Theme.of(context)
+                                                        .primaryColor)),
+                                          ),
+                                  ),
+                                  SizedBox(width: 10),
+                                  Container(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 12, vertical: 3),
+                                      decoration: BoxDecoration(
+                                          color:
+                                          _con.restaurant.reservation_active
+                                              ? Colors.green
+                                              : Colors.red,
+                                          borderRadius:
+                                          BorderRadius.circular(24)),
+                                      child: Text(
+                                        S.of(context).reservation,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .caption
+                                            .merge(TextStyle(
+                                            color: Theme.of(context)
+                                                .primaryColor)),
+                                      )),
+                                  Expanded(child: SizedBox(height: 0)),
+                                  Container(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 12, vertical: 3),
+                                    decoration: BoxDecoration(
+                                        color: Theme.of(context)
+                                            .accentColor
+                                            .withOpacity(0.9),
+                                        borderRadius:
+                                            BorderRadius.circular(24)),
+                                    child: Text(
+                                      _con.restaurant.distance != null
+                                          ? _con.restaurant.distance.substring(
+                                              0,
+                                              _con.restaurant.distance
+                                                  .lastIndexOf(' '))
+                                          : '0.00 km',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .caption
+                                          .merge(TextStyle(
+                                              color: Theme.of(context)
+                                                  .primaryColor)),
+                                    ),
+                                  ),
+                                  SizedBox(width: 20),
+                                ],
+                              ),
+                              /*Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 20, vertical: 12),
+                                child: Helper.applyHtml(
+                                    context, _con.restaurant.description),
+                              ),*/
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 20),
+                                child: ListTile(
+                                  dense: true,
+                                  contentPadding:
+                                      EdgeInsets.symmetric(vertical: 0),
+                                  leading: Icon(
+                                    Icons.stars,
+                                    color: Theme.of(context).hintColor,
+                                  ),
+                                  title: Text(
+                                    S.of(context).information,
+                                    style:
+                                        Theme.of(context).textTheme.headline4,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 20, vertical: 12),
+                                child: Helper.applyHtml(
+                                    context, _con.restaurant.getTimeInfo()),
+                              ),
+                              Container(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 20, vertical: 20),
+                                margin: const EdgeInsets.symmetric(vertical: 5),
+                                color: Theme.of(context).primaryColor,
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: <Widget>[
+                                    Expanded(
+                                      child: Text(
+                                        _con.restaurant.address ?? '',
+                                        overflow: TextOverflow.ellipsis,
+                                        maxLines: 2,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText1,
+                                      ),
+                                    ),
+                                    SizedBox(width: 10),
+                                    SizedBox(
+                                      width: 42,
+                                      height: 42,
+                                      child: FlatButton(
+                                        padding: EdgeInsets.all(0),
+                                        onPressed: () {
+                                          /*Navigator.of(context).pushNamed(
+                                              '/Pages',
+                                              arguments: new RouteArgument(
+                                                  id: '1',
+                                                  param: _con.restaurant));*/
+                                        },
+                                        child: Icon(
+                                          Icons.directions,
+                                          color: Theme.of(context).primaryColor,
+                                          size: 24,
+                                        ),
+                                        color: Theme.of(context)
+                                            .accentColor
+                                            .withOpacity(0.9),
+                                        shape: StadiumBorder(),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 20, vertical: 20),
+                                margin: const EdgeInsets.symmetric(vertical: 5),
+                                color: Theme.of(context).primaryColor,
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Expanded(
+                                      child: Text(
+                                        '${_con.restaurant.contact_number}',
+                                        overflow: TextOverflow.ellipsis,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText1,
+                                      ),
+                                    ),
+                                    SizedBox(width: 10),
+                                    SizedBox(
+                                      width: 42,
+                                      height: 42,
+                                      child: FlatButton(
+                                        padding: EdgeInsets.all(0),
+                                        onPressed: () {
+                                          launch(
+                                              "tel:${_con.restaurant.contact_number}");
+                                        },
+                                        child: Icon(
+                                          Icons.call,
+                                          color: Theme.of(context).primaryColor,
+                                          size: 24,
+                                        ),
+                                        color: Theme.of(context)
+                                            .accentColor
+                                            .withOpacity(0.9),
+                                        shape: StadiumBorder(),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Positioned(
+                      top: 32,
+                      right: 20,
+                      child: new IconButton(
+                        icon: new Icon(Icons.search,
+                            color: Theme.of(context).hintColor),
+                        onPressed: () =>
+                            Navigator.of(context).push(SearchModal()),
+                      ),
+                    ),
+                  ],
+                ),
+        ));
+  }
+}
